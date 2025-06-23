39c59dd2f9712b524e3981282b4f14b74214aa42
Malakpro712 <gleb712.habib512@gmail.com>
2025-06-23 19:00:20 +0500

    Update4

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private int currentLevel = 0;
+    void Update()
+    {
+        if (checkFinish())
+        {
+            currentLevel++;
+            NextLevel();
+        }
+    }
+
+    bool checkFinish()
+    {
+        GameObject[] finishs = GameObject.FindGameObjectsWithTag("Finish");
+        return finishs.Length == 0;
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+}
